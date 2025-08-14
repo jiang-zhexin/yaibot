46c35f59de7b3be762de8405a43c29d3cc9a5a79
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-08-14 15:19:56 +0800

    fixes and improvements

--- src/type.ts
+++ src/type.ts
@@ -2,7 +2,4 @@ import { Context } from "grammy"
 
 declare global {
     type MyContext = Context & { payload: string }
-    type notNull<T> = {
-        [K in keyof T]: Exclude<T[K], null>
-    }
 }
